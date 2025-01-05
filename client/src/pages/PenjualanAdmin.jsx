251d548c9f538bd94041c86440babcbb42eebdbd
Farkhanisme <farkhanmahmud@gmail.com>
2025-01-05 19:35:17 +0700

    1.3.2

--- client/src/pages/PenjualanAdmin.jsx
+++ client/src/pages/PenjualanAdmin.jsx
@@ -4,6 +4,7 @@ import moment from "moment";
 import { formatRupiah } from "../functions/util";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
+import toast, { Toaster } from "react-hot-toast";
 
 const PenjualanAdmin = () => {
   const [penjualan, setPenjualan] = useState([]);
@@ -81,10 +82,21 @@ const PenjualanAdmin = () => {
         ]),
       });
 
-      // Simpan PDF
-      pdf.save("laporan_penjualan.pdf");
+      pdf.autoTable({
+        startY: pdf.lastAutoTable.finalY + 10, // Mulai setelah tabel sebelumnya
+        head: [["No.", "Pendapatan", "Setor", "Laba"]],
+        body: allTotal.map((allTotal, index) => [
+          index + 1,
+          formatRupiah(allTotal.totalTerjual),
+          formatRupiah(allTotal.totalSetor),
+          formatRupiah(allTotal.totalTerjual - allTotal.totalSetor),
+        ]),
+      });
 
-      alert("PDF berhasil diunduh!");
+      // Simpan PDF
+      pdf.save(`laporan_penjualan_${moment().format("DD-MM-YY")}.pdf`);
+
+      toast.success("PDF berhasil diunduh!");
     } catch (error) {
       console.error("Error saat membuat PDF:", error);
       alert("Terjadi kesalahan saat membuat PDF.");
@@ -93,47 +105,14 @@ const PenjualanAdmin = () => {
 
   return (
     <>
+      <Toaster />
       <button
         className="md:w-fit w-full p-2 bg-green-500 rounded-md text-white mb-5"
         onClick={handleGeneratePDF}
       >
-        Generate PDF dan Kirim ke WhatsApp
+        Download Laporan Penjualan
       </button>
-      <div className="flex flex-col-reverse md:flex-row mb-5">
-        <table className="w-full bg-zinc-100 text-center">
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Tanggal</th>
-              <th>Nama Barang</th>
-              <th>Qty</th>
-              <th>Sisa</th>
-              <th>Harga</th>
-              <th>Penitip</th>
-            </tr>
-          </thead>
-          <tbody>
-            {penjualan.map((penjualan, index) => (
-              <tr key={index} className="text-center border border-x-0">
-                <td>{index + 1}</td>
-                <td>
-                  {moment(penjualan.tanggal).local().format("DDD MMMM, YYYY")}
-                </td>
-                <td>{penjualan.namaBarang}</td>
-                <td>{penjualan.totalTerjual}</td>
-                <td>
-                  {penjualan.stock != null
-                    ? penjualan.stock - penjualan.totalTerjual
-                    : "♾️"}
-                </td>
-                <td>{formatRupiah(penjualan.totalHarga)}</td>
-                <td>{penjualan.nama}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="flex md:flex-row flex-col md:space-y-0 space-y-5">
+      <div className="flex md:flex-row flex-col md:space-y-0 space-y-5 space-x-0 md:space-x-5 mb-5">
         <div className="flex flex-col md:w-1/2 w-full space-y-2">
           <table className="bg-zinc-100 text-center table-auto">
             <thead>
@@ -193,6 +172,40 @@ const PenjualanAdmin = () => {
                   {formatRupiah(setor.totalSetor)}
                 </td>
                 <td className="text-center border border-x-0">{setor.nama}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex flex-col-reverse md:flex-row">
+        <table className="w-full bg-zinc-100 text-center">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Tanggal</th>
+              <th>Nama Barang</th>
+              <th>Qty</th>
+              <th>Sisa</th>
+              <th>Harga</th>
+              <th>Penitip</th>
+            </tr>
+          </thead>
+          <tbody>
+            {penjualan.map((penjualan, index) => (
+              <tr key={index} className="text-center border border-x-0">
+                <td>{index + 1}</td>
+                <td>
+                  {moment(penjualan.tanggal).local().format("DDD MMMM, YYYY")}
+                </td>
+                <td>{penjualan.namaBarang}</td>
+                <td>{penjualan.totalTerjual}</td>
+                <td>
+                  {penjualan.stock != null
+                    ? penjualan.stock - penjualan.totalTerjual
+                    : "♾️"}
+                </td>
+                <td>{formatRupiah(penjualan.totalHarga)}</td>
+                <td>{penjualan.nama}</td>
               </tr>
             ))}
           </tbody>
